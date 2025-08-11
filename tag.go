e9d07ad4cc7c118ff5e6fb0d30ee9c846c8b4084
szks-repo <szkscookie@gmail.com>
2025-08-12 03:00:38 +0900

    impl

--- tag.go
+++ tag.go
@@ -1,22 +1,36 @@
 package tag
 
-import "reflect"
+import (
+	"reflect"
+)
 
-// StructTags represents a collection of tags for struct fields.
+// StructTag represents a collection of tags for struct fields.
 //
 //	struct User {
 //	    Name string `json:"name,omitempty" form:"name"`
 //	}
 //
 // It allows for easy access to tags by namespace, such as "json" or "form".
-type StructTags struct {
+type StructTag struct {
 	tag        reflect.StructTag
-	namespaces map[string]string
+	namespaces map[string]Item
 }
 
-func NewTagsFromField(field reflect.StructField) *StructTags {
-	return &StructTags{
+func NewTagsFromField(field reflect.StructField) *StructTag {
+	st := &StructTag{
 		tag:        field.Tag,
-		namespaces: make(map[string]string),
+		namespaces: make(map[string]Item),
 	}
+	if field.Tag != "" {
+
+	}
+	return st
+}
+
+type Item struct {
+	namespace string
+}
+
+func parseTag(s string) (string, Item) {
+	return "", Item{}
 }
