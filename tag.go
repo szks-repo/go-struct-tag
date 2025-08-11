ef06d1997f30c248d6e8b2efc2dbe1ac1983ce88
szks-repo <57564716+szks-repo@users.noreply.github.com>
2025-08-11 23:09:55 +0900

    update: design

--- tag.go
+++ tag.go
@@ -2,12 +2,14 @@ package tag
 
 import "reflect"
 
-type StructTag struct {
-	tag reflect.StructTag
+type StructTags struct {
+	tag        reflect.StructTag
+	namespaces map[string]string
 }
 
-func NewFromField(field reflect.StructField) *StructTag {
-	return &StructTag{
-		tag: field.Tag,
+func NewFromField(field reflect.StructField) *StructTags {
+	return &StructTags{
+		tag:        field.Tag,
+		namespaces: make(map[string]string),
 	}
 }
